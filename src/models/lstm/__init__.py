2e6293526491dfdf73bafa318ad0512630f631a2
Amadeus <serzhik.panchenko@gmail.com>
2026-05-07 21:47:01 +0300

    Добавил новые LSTM-файлы в чистый слой:
    src/models/lstm/dataset.py
    src/models/lstm/network.py
    В dataset.py перенесены reusable части:
    
    SequenceSampleIndex
    SequenceStandardizer
    SequenceDataset
    build_history_by_symbol
    В network.py:
    
    LSTMClassifier
    Обновил только новый:
    
    src/models/lstm/__init__.py
    Теперь можно импортировать так:
    
    from src.models.lstm import LSTMClassifier, SequenceData

--- src/models/lstm/__init__.py
+++ src/models/lstm/__init__.py
@@ -1,4 +1,14 @@
+from src.models.lstm.dataset import SequenceDataset, SequenceSampleIndex, SequenceStandardizer, build_history_by_symbol
+from src.models.lstm.network import LSTMClassifier
 from src.models.lstm.runner import LstmRunner
 from src.models.lstm.spec import SPEC
 
-__all__ = ["LstmRunner", "SPEC"]
+__all__ = [
+    "LSTMClassifier",
+    "LstmRunner",
+    "SequenceDataset",
+    "SequenceSampleIndex",
+    "SequenceStandardizer",
+    "SPEC",
+    "build_history_by_symbol",
+]
